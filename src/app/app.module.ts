0c1383e19cce6b45284043bb3523bf07f53f0866
athulprakash007 <athull362@gmail.com>
2022-11-22 15:17:13 -0800

    c

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FriendComponent } from './friend/friend.component';
+import { ViewallfriendComponent } from './viewallfriend/viewallfriend.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const myroutes:Routes=[
+
+{path:"",component: FriendComponent},
+{path:"viewallfriend",component: ViewallfriendComponent}
+
+]
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FriendComponent,
+    ViewallfriendComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myroutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
